008615346a70f6a1ab51f6dc1b0cc18ead9b2ce3
riigarXX <alvarofernandotorres@gmail.com>
2022-04-16 19:52:41 +0200

    adding validations

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,38 +2,79 @@
   <a-row>
     <a-col :xs="{ span: 24 }" :sm="{ span: 12, offset: 6 }">
       <a-form
-        @submit.prevent="handleSubmit"
         name="basicLogin"
-        autcomplete="off"
+        layout="vertical"
+        autocomplete="off"
+        :model="formState"
+        @finish="onFinish"
+        @finishFailed="onFinishFailed"
       >
-        <input type="email" placeholder="Ingrese email" v-model.trim="email" />
-        <input
-          type="password"
-          placeholder="Ingrese su contraseña"
-          v-model.trim="password"
-        />
-        <button type="submit" :disabled="userStore.loadingUser">
-          Login en firebase!
-        </button>
+        <a-form-item
+          name="email"
+          label="Ingresa tu correo"
+          :rules="[
+            {
+              required: true,
+              whitespace: true,
+              message: 'Ingresa un email',
+              type: 'email',
+            },
+          ]"
+        >
+          <a-input v-model:value="formState.email" />
+        </a-form-item>
+        <a-form-item
+          name="password"
+          label="Ingrese contraseña"
+          :rules="[
+            {
+              required: true,
+              min: 6,
+              max: 24,
+              whitespace: true,
+              message:
+                'Ingresa tu contraseña con minimo 6 carácteres y un maximo de 24',
+            },
+          ]"
+        >
+          <a-input-password v-model:value="formState.password" />
+        </a-form-item>
+        <a-form-item>
+          <a-button
+            type="primary"
+            html-type="submit"
+            :disabled="userStore.loadingUser"
+          >
+            Login en firebase!
+          </a-button>
+        </a-form-item>
       </a-form>
     </a-col>
   </a-row>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
 import { useUserStore } from "../stores/user";
 
 const userStore = useUserStore();
 
-const email = ref("");
-const password = ref("");
+const formState = reactive({
+  email: "",
+  password: "",
+});
 
 const handleSubmit = async () => {
   if (!email.value || password.value.length < 5) {
     return alert("llena los campos");
   }
-  await userStore.loginUser(email.value, password.value);
+  await userStore.loginUser(formState.email, formState.password);
+};
+const onFinish = async (values) => {
+  console.log("Success", values);
+};
+const onFinishFailed = async (values) => {
+  console.log("Failed", values);
 };
 </script>
 
