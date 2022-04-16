4f66bd5619ea89e419688b8edc0a4e00cad631f0
rigarXX <alvarofernandotorres@gmail.com>
2022-04-16 15:46:00 +0200

    add ui framework valid with vue3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>App</h1>
+    <a-button type="primary">Button</a-button>
     <nav v-if="!userStore.loadingSession">
       <router-link to="/" v-if="userStore.userData">Home</router-link> |
       <router-link to="/login" v-if="!userStore.userData">Login</router-link> |
